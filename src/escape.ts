8043dbdf9ad993f5d4f60d6ca6ed60c0be33812e
Lumi Kalt <lumi.mia.kalt@gmail.com>
2022-09-04 10:52:13 +0100

    Fix rainbow text

--- src/escape.ts
+++ src/escape.ts
@@ -1,56 +1,21 @@
 // see escape-latex
-// https://github.com/dangmai/escape-latex/blob/master/src/index.js
+// https,//github.com/dangmai/escape-latex/blob/master/src/index.js
 
-const defaultEscapes = {
-	"{": "\\{",
-	"}": "\\}",
-	"\\": "\\textbackslash{}",
-	"#": "\\#",
-	$: "\\$",
-	"%": "\\%",
-	"&": "\\&",
-	"^": "\\textasciicircum{}",
-	_: "\\_",
-	"~": "\\textasciitilde{}",
-};
+const needsEscape = ["{", "}", "\\", "#", "$", "%", "&", "^", "_", "~"];
 
-const defaultEscapeMapFn = (defaultEscapes: any) =>
-	Object.assign({}, defaultEscapes);
+const defaultEscapes = new Map([
+	["{", "\\{"],
+	["}", "\\}"],
+	["\\", "\\textbackslash"],
+	["#", "\\#"],
+	["$", "\\textdollar"],
+	["%", "\\%"],
+	["&", "\\&"],
+	["^", "\\textasciicircum"],
+	["_", "\\_"],
+	["~", "\\textasciitilde"],
+]);
 
-const lescape = (
-	str: string,
-	{ preserveFormatting = false, escapeMapFn = defaultEscapeMapFn } = {}
-) => {
-	let runningStr = String(str);
-	let result = "";
-
-	const escapes = escapeMapFn(Object.assign({}));
-	const escapeKeys = Object.keys(escapes); // as it is reused later on
-
-	// Algorithm: Go through the string character by character, if it matches
-	// with one of the special characters then we'll replace it with the escaped
-	// version.
-	while (runningStr) {
-		let specialCharFound = false;
-		escapeKeys.forEach(function (key, index) {
-			if (specialCharFound) {
-				return;
-			}
-			if (
-				runningStr.length >= key.length &&
-				runningStr.slice(0, key.length) === key
-			) {
-				result += escapes[escapeKeys[index]];
-				runningStr = runningStr.slice(key.length, runningStr.length);
-				specialCharFound = true;
-			}
-		});
-		if (!specialCharFound) {
-			result += runningStr.slice(0, 1);
-			runningStr = runningStr.slice(1, runningStr.length);
-		}
-	}
-	return result;
-};
+const lescape = (c: string) => defaultEscapes.get(c) ?? c;
 
 export default lescape;
