0e061790a6beebe467cc645f54e1dcc2591e2d84
Lumi Kalt <lumi.mia.kalt@gmail.com>
2022-09-27 16:03:23 +0100

    Log command usage for debugging

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { Client } from "revolt.js";
 import { commands } from "./commands";
 import { cat } from "./lib/owo";
 
-const prefix = process.env.PREFIX || "!!!"; // amazing prefix!
+const prefix = process.env.PREFIX || "real!"; // amazing prefix!
 
 let client = new Client();
 
@@ -18,15 +18,17 @@ client.on("message", async msg => {
   if (
     msg.author_id === client.user?._id &&
     (await cat(msg.content)) !== msg.content
-  ) {
+  )
     msg.edit({ content: await cat(msg.content) });
-  } else if (!msg.content.startsWith(prefix) || msg.content.length >= 2000)
+  else if (!msg.content.startsWith(prefix) || msg.content.length >= 2000)
     return;
 
   let splitted = msg.content.substring(5).split(" ");
+  console.log(splitted);
   let command = commands.get(splitted.shift() ?? "") ?? ((_: string) => null);
   let args = splitted.join(" ");
   let res: string | null = await command(args);
+  console.log(res);
 
   if (res && res.length <= 2000) {
     if (msg.author_id === client.user?._id) msg.edit({ content: res });
